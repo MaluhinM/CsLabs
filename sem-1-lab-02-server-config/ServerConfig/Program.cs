8fb29eb5a5bb07a6a32f5d169706d46fba748490
MaluhinM <mmaluhin2008@gmail.com>
2026-09-25 19:16:43 +0500

    lab-02

--- sem-1-lab-02-server-config/ServerConfig/Program.cs
+++ sem-1-lab-02-server-config/ServerConfig/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace ServerConfig;
+
+class Program
+{
+    static (bool running, bool restoring) GetRandomInfo()
+    {
+        bool running = Random.Shared.Next(2) == 0;
+        bool restoring = Random.Shared.Next(2) == 0;
+
+        return (running, restoring);
+    }
+
+    public static void Main()
+    {
+        (bool running, bool restoring) = GetRandomInfo();
+        Console.WriteLine(running);
+        Console.WriteLine(restoring);
+    }
+}
