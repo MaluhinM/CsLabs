6e099aa7a7bcf00978919ee5e689a4928d151adb
MXRMVL <mmaluhin2008@gmail.com>
2026-09-26 14:35:07 +0500

    For the future

--- Practice/2026.09.26/Test/Program.cs
+++ Practice/2026.09.26/Test/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Test;
+
+class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            
+        }
+    }
+}
